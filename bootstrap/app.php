f3ba718494fac4e86980dae6defdcf3cadf2416d
magerishere <immagerishere@gmail.com>
2025-02-09 21:12:18 +0330

    feat(app): change response error validations when is api request in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +15,16 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $e, Request $request) {
+            logger(get_class($e));
+            logger($e->getMessage());
+            if (isApiRequest(request: $request)) {
+                return match (true) {
+                    $e instanceof \Illuminate\Validation\ValidationException => apiResponse(data: [
+                        'errors' => $e->errors(),
+                    ], message: $e, status: \Symfony\Component\HttpFoundation\Response::HTTP_UNPROCESSABLE_ENTITY),
+                    default => apiResponse(data: [], message: $e)
+                };
+            }
+        });
     })->create();
