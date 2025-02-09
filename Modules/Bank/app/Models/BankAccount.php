cd24a4962ef74fb242028de6027e83c0580c67ec
magerishere <immagerishere@gmail.com>
2025-02-09 16:16:50 +0330

    feat(bank account): add BankAccountFactory

--- Modules/Bank/app/Models/BankAccount.php
+++ Modules/Bank/app/Models/BankAccount.php
@@ -2,12 +2,16 @@
 
 namespace Modules\Bank\Models;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Bank\Database\Factories\BankAccountFactory;
 use Modules\Bank\Enums\BankAccountStatus;
 use Modules\Bank\Enums\BankAccountType;
 
 class BankAccount extends Model
 {
+    use HasFactory;
     public $incrementing = false;
     protected $keyType = 'string';
     protected $primaryKey = 'number';
@@ -27,5 +31,10 @@ class BankAccount extends Model
             'status' => BankAccountStatus::class,
             'type' => BankAccountType::class,
         ];
+    }
+
+    protected static function newFactory(): Factory
+    {
+        return BankAccountFactory::new();
     }
 }
---
+++ Modules/Bank/database/factories/BankAccountFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Bank\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\Bank\Enums\BankAccountStatus;
+use Modules\Bank\Enums\BankAccountType;
+use Modules\Bank\Models\BankAccount;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Modules\Bank\Models\Bank>
+ */
+class BankAccountFactory extends Factory
+{
+    protected $model = BankAccount::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'number' => fake()->numerify('##################'),
+            'status' => fake()->randomElement(BankAccountStatus::cases()),
+            'type' => fake()->randomElement(BankAccountType::cases()),
+            'balance' => fake()->numberBetween(0, 1000000000),
+        ];
+    }
+}
