3782084c0871ed7e139533ad0a4c640c09e2024f
magerishere <immagerishere@gmail.com>
2025-02-10 13:53:25 +0330

    refactor(bank): remove unused routes

--- Modules/Bank/routes/web.php
+++ Modules/Bank/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Bank\Http\Controllers\V1\Api\BankController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +14,4 @@ use Modules\Bank\Http\Controllers\V1\Api\BankController;
 */
 
 Route::group([], function () {
-    Route::resource('bank', BankController::class)->names('bank');
 });
