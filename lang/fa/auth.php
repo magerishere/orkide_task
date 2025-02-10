36151e48eb34b21d88a034b4ade9355b099ad552
magerishere <immagerishere@gmail.com>
2025-02-10 16:37:50 +0330

    chore(validation): add persian validation ( lang:publish )

---
+++ lang/fa/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'این اطلاعات ورود با سوابق ما مطابقت ندارد.',
+    'password' => 'رمز عبور ارائه‌شده نادرست است.',
+    'throttle' => 'تعداد زیادی تلاش ناموفق برای ورود انجام شده است. لطفاً :seconds ثانیه دیگر تلاش کنید.',
+
+];
---
+++ lang/fa/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'رمز عبور شما بازنشانی شد.',
+    'sent' => 'ما لینک بازنشانی رمز عبور را به ایمیل شما ارسال کردیم.',
+    'throttled' => 'لطفاً قبل از تلاش مجدد صبر کنید.',
+    'token' => 'این توکن بازنشانی رمز عبور نامعتبر است.',
+    'user' => "کاربری با این آدرس ایمیل پیدا نشد.",
+
+];
