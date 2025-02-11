8295d30fb6af6e7ae286eae911942fdc634ad4ad
magerishere <immagerishere@gmail.com>
2025-02-11 09:40:25 +0330

    style(bank): remove unused methods in BankController

--- Modules/Bank/app/Http/Controllers/V1/Api/BankController.php
+++ Modules/Bank/app/Http/Controllers/V1/Api/BankController.php
@@ -4,7 +4,6 @@ namespace Modules\Bank\Http\Controllers\V1\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Modules\Bank\Enums\BankAccountStatus;
 use Modules\Bank\Exceptions\BankAccountBalanceNotEnoughException;
@@ -23,62 +22,6 @@ use Modules\Transaction\Repository\Contracts\TransactionRepositoryInterface;
 
 class BankController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return view('bank::index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('bank::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
-    {
-        return view('bank::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('bank::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function cardToCard(
         BankCardToCardRequest $request,
         User $user,
